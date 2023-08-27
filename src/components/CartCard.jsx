4a0f2fe603e3cb84c0433ce871ca0dd27922cf61
NikzZz <santillan.nj.work@gmail.com>
2023-08-27 19:42:57 +0800

    Add static details in CartCard

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -5,25 +5,40 @@ function CartCard() {
   const data = useLocation();
   const product = data.state[0];
   const user = data.state[1];
+  const quantity = 1;
   console.log(product);
   console.log(user);
+
+  const placeOrder = async () => {
+
+  }
 
   return (
     <div>
       <div className="card w-25">
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="card-link">
-            Card link
-          </a>
-          <a href="#" className="card-link">
-            Another link
-          </a>
+          <h5 className="card-title">Order Summary</h5>
+          <h6 className="card-subtitle mb-2 text-body-secondary">
+            Card subtitle
+          </h6>
+          <div>
+            <p className="card-text">Price</p>
+            <p className="card-text">₱{USformatter.format(product.Price)}</p>
+          </div>
+          <div>
+            <p className="card-text">Quantity</p>
+            <p className="card-text">{quantity}</p> 
+          </div>
+          <div>
+            <p className="card-text">Shipping</p>
+            <p className="card-text">Free</p>
+          </div>
+          <hr />
+          <div>
+            <p className="card-text">Total</p>
+            <p className="card-text">₱{USformatter.format(product.Price * quantity)}</p>
+          </div>
+          <button type="submit" onClick={placeOrder}></button>
         </div>
       </div>
     </div>
