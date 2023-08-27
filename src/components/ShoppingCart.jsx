967d401f37ca319d6a4a4ca2240bfa84533a49fb
Kylle <kylle_talan22@yahoo.com>
2023-08-27 17:15:18 +0800

    Minor Changes Added

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,89 +1,124 @@
 import React, { useState } from "react";
 
-const products = [
-  { id: 1, name: "Product A", price: 10 },
-  { id: 2, name: "Product B", price: 20 },
-  { id: 3, name: "Product C", price: 15 },
+const laptops = [
+  {
+    id: 1,
+    name: "Laptop A",
+    price: 800,
+    description: "Powerful laptop with high performance.",
+    colors: ["Black", "White", "Titanium Silver"],
+  },
+  {
+    id: 2,
+    name: "Laptop B",
+    price: 1200,
+    description: "Sleek and lightweight laptop for professionals.",
+    colors: ["Black", "White", "Titanium Silver"],
+  },
+  {
+    id: 3,
+    name: "Laptop C",
+    price: 1500,
+    description: "Gaming laptop with advanced graphics capabilities.",
+    colors: ["Black", "White", "Titanium Silver"],
+  },
 ];
 
-const Product = ({ product, addToCart }) => {
+const Laptop = ({ laptop, addToCart }) => {
   const [quantity, setQuantity] = useState(1);
+  const [selectedColor, setSelectedColor] = useState(laptop.colors[0]);
 
   const handleQuantityChange = (event) => {
     setQuantity(parseInt(event.target.value, 10));
   };
 
+  const handleColorChange = (event) => {
+    setSelectedColor(event.target.value);
+  };
+
   return (
-    <div className="product">
-      <h3>{product.name}</h3>
-      <p>${product.price}</p>
+    <div className="laptop">
+      <h3>{laptop.name}</h3>
+      <p>${laptop.price}</p>
+      <p>{laptop.description}</p>
+      <select value={selectedColor} onChange={handleColorChange}>
+        {laptop.colors.map((color, index) => (
+          <option key={index} value={color}>
+            {color}
+          </option>
+        ))}
+      </select>
       <input
         type="number"
         min="1"
         value={quantity}
         onChange={handleQuantityChange}
       />
-      <button onClick={() => addToCart(product, quantity)}>Add to Cart</button>
+      <button onClick={() => addToCart(laptop, selectedColor, quantity)}>
+        Add to Cart
+      </button>
     </div>
   );
 };
 
 const CartItem = ({ item, removeFromCart }) => {
-  const { product, quantity } = item;
+  const { laptop, color, quantity } = item;
 
   return (
     <div className="cart-item">
-      <h4>{product.name}</h4>
+      <h4>{laptop.name}</h4>
+      <p>Color: {color}</p>
       <p>Quantity: {quantity}</p>
-      <p>Price: ${product.price}</p>
-      <p>Total: ${product.price * quantity}</p>
-      <button onClick={() => removeFromCart(product)}>Remove</button>
+      <p>Price: ${laptop.price}</p>
+      <p>Total: ${laptop.price * quantity}</p>
+      <p>Description: {laptop.description}</p>
+      <button onClick={() => removeFromCart(laptop, color)}>Remove</button>
     </div>
   );
 };
 
-const ShoppingCart = () => {
+const LaptopShoppingCart = () => {
   const [cartItems, setCartItems] = useState([]);
 
-  const addToCart = (product, quantity) => {
+  const addToCart = (laptop, color, quantity) => {
     const existingItem = cartItems.find(
-      (item) => item.product.id === product.id
+      (item) => item.laptop.id === laptop.id && item.color === color
     );
 
     if (existingItem) {
       const updatedCart = cartItems.map((item) =>
-        item.product.id === product.id
+        item.laptop.id === laptop.id && item.color === color
           ? { ...item, quantity: item.quantity + quantity }
           : item
       );
       setCartItems(updatedCart);
     } else {
-      setCartItems([...cartItems, { product, quantity }]);
+      setCartItems([...cartItems, { laptop, color, quantity }]);
     }
   };
 
-  const removeFromCart = (product) => {
+  const removeFromCart = (laptop, color) => {
     const updatedCart = cartItems.filter(
-      (item) => item.product.id !== product.id
+      (item) => item.laptop.id !== laptop.id || item.color !== color
     );
     setCartItems(updatedCart);
   };
 
   const calculateCartTotal = () => {
     return cartItems.reduce(
-      (total, item) => total + item.product.price * item.quantity,
+      (total, item) => total + item.laptop.price * item.quantity,
       0
     );
   };
 
   return (
     <div>
-      <h1>Shopping Cart</h1>
-      <div className="product-list">
-        <h2>Products</h2>
-        <div className="products">
-          {products.map((product) => (
-            <Product key={product.id} product={product} addToCart={addToCart} />
+      <h1>Laptop Shopping Cart</h1>
+      <div className="laptop-list">
+        <h2>Laptops</h2>
+        <div className="laptops">
+          {laptops.map((laptop) => (
+            <Laptop key={laptop.id} laptop={laptop} addToCart={addToCart} />
           ))}
         </div>
       </div>
@@ -100,4 +135,4 @@ const ShoppingCart = () => {
   );
 };
 
-export default ShoppingCart;
+export default LaptopShoppingCart;
